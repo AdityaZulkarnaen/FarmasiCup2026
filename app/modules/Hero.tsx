bcbba5f0529af98e8602b6241ffe1c0cf26805c3
AdityaZulkarnaen <adityazulkarnaen083@gmail.com>
2026-05-15 19:22:44 +0700

    feat: set up project

---
+++ app/modules/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='bg-[#422351] w-screen h-screen flex items-center justify-center'>
+        <div className='w-[75%] h-[88.5%] bg-[#BE3A85] rounded-[50%] mt-40 blur-[125px]'></div>
+    </div>
+  )
+}
+
+export default Hero
